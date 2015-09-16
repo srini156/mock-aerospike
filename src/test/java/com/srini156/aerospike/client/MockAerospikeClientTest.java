7c7edcd1bd302d0c65eed3599cdd1bf068234cf3
Srinivas Iyengar <srinivas.iyengar@olacabs.com>
2015-09-16 12:49:59 +0530

    Added tests for both AerospikeClient and MockAerospikeClient

--- src/test/java/com/srini156/aerospike/client/MockAerospikeClientTest.java
+++ src/test/java/com/srini156/aerospike/client/MockAerospikeClientTest.java
@@ -1,56 +1,21 @@
 package com.srini156.aerospike.client;
 
-import static org.testng.Assert.assertFalse;
-import static org.testng.Assert.assertNotNull;
-import static org.testng.Assert.assertNull;
-import static org.testng.Assert.assertTrue;
-
 import org.srini156.aerospike.client.MockAerospikeClient;
-import org.testng.annotations.Test;
-
-import com.aerospike.client.Bin;
-import com.aerospike.client.IAerospikeClient;
-import com.aerospike.client.Key;
-import com.aerospike.client.Record;
 
 /**
+ * Tests to be run against MockAerospikeClient to ensure functionality of IAerospikeClient is
+ * implemented.
+ * 
  * @author srinivas.iyengar
  *
  */
-public class MockAerospikeClientTest {
+public class MockAerospikeClientTest extends BaseAerospikeTest {
 
-	private IAerospikeClient mockAerospikeClient = new MockAerospikeClient();
-	private String namespace = "test";
-	private String set = "test_set";
-	private Key key = new Key(namespace, set, "Key");
-	private Bin[] bins = new Bin[] { new Bin("first", "first"), new Bin("second", 123L) };
-
-	@Test
-	public void shouldPutRecord() {
-		mockAerospikeClient.put(null, key, bins);
+	/**
+	 * Default Constructor.
+	 */
+	public MockAerospikeClientTest() {
+		super(new MockAerospikeClient());
 	}
 
-	@Test(dependsOnMethods = "shouldPutRecord")
-	public void shouldGetRecord() {
-		Record record = mockAerospikeClient.get(null, key);
-		assertNotNull(record);
-	}
-
-	@Test(dependsOnMethods = "shouldGetRecord")
-	public void shouldDeleteRecord() {
-		assertTrue(mockAerospikeClient.delete(null, key));
-	}
-
-	@Test(dependsOnMethods = "shouldDeleteRecord")
-	public void shouldNotGetRecord() {
-		Record record = mockAerospikeClient.get(null, key);
-		assertNull(record);
-	}
-
-	@Test
-	public void shouldNotDeleteNonExistingRecord() {
-		Key nonExistingKey = new Key(namespace, set, "Non-key");
-		assertFalse(mockAerospikeClient.delete(null, nonExistingKey));
-
-	}
 }
